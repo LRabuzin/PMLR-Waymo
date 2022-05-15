d42e305aac4761668ebb9faff75fa8930fe02810
LRabuzin <lovro.rabuzin@gmail.com>
2022-05-15 19:06:44 +0200

    Added validation code

---
+++ scripts/eval_utils.py
@@ -0,0 +1,15 @@
+import torch
+
+def iou_single_class(predictions, targets, class_id):
+    predictions_max = predictions.argmax(1)
+    predictions_bool = predictions_max == class_id
+    targets_bool = targets == class_id
+    union = torch.logical_or(predictions_bool, targets_bool)
+    intersection = torch.logical_and(predictions_bool, targets_bool)
+    return torch.sum(intersection)/torch.sum(union)
+
+def mean_iou(predictions, targets, no_classes = 23):
+    acc_iou = 0
+    for class_id in range(no_classes):
+        acc_iou += iou_single_class(predictions, targets, class_id)
+    return acc_iou/no_classes
--- scripts/training.py
+++ scripts/training.py
@@ -4,7 +4,7 @@ sys.path.append('/cluster/home/lrabuzin/PMLR-Waymo/dataset')
 sys.path.append('/cluster/home/lrabuzin/PMLR-Waymo/models')
 
 import wandb
-
+import eval_utils
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -13,54 +13,83 @@ from torch.utils.data import DataLoader
 import segmentation_dataset
 import minkunet
 
+
+
+def validate_model(model, valid_dl, loss_func, no_classes=22, device='cpu'):
+    "Compute performance of the model on the validation dataset"
+    model.eval()
+
+    val_loss = 0.
+    total_out = []
+    total_labels = []
+
+    with torch.inference_mode():
+        valid_iter = iter(valid_dl)
+        for i, data in enumerate(valid_iter):
+            coords, feats, labels = data
+            out = model(ME.SparseTensor(feats, coords, device = device))
+
+            total_out.append(out)
+            total_labels.append(labels)
+
+            val_loss += loss_func(out.F.squeeze(), labels.long().to(device)).item()
+
+    total_out = torch.cat(total_out)
+    total_labels = torch.cat(total_labels)
+
+    iou_scores = [eval_utils.iou_single_class(total_out, total_labels, class_id, no_classes) for class_id in range(no_classes)]
+    iou_dict = dict(enumerate(iou_scores))
+
+    return val_loss / len(valid_dl), iou_dict
+
 if __name__ == "__main__":
     wandb.init(project="pmlr-waymo", entity="lrabuzin")
-
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    # print(f'Device: {device}')
-    train_dataset = segmentation_dataset.WaymoSegmentationDataset(root_dir='/cluster/scratch/lrabuzin/waymo_frames', device=device)
-    train_dataloader = DataLoader(
-        train_dataset,
-        batch_size=5,
-        collate_fn=ME.utils.SparseCollation(),
-        num_workers=0,
-        shuffle = True)
-
-    criterion = nn.CrossEntropyLoss(reduction='mean')
-    net = minkunet.MinkUNet14A(in_channels=3, out_channels=23, D=3)
-    net = net.to(device)
-
-    optimizer = optim.SGD(
-        net.parameters(),
-        lr=0.001,
-        momentum=0.9)
-    
     wandb.config = {
         "learning_rate": 0.001,
         "epochs": 10,
         "batch_size": 5,
         "momentum" : 0.9
     }
+    config = wandb.config
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    train_dataset = segmentation_dataset.WaymoSegmentationDataset(root_dir='/cluster/scratch/lrabuzin/waymo_frames', device=device)
+    train_dataloader = DataLoader(
+        train_dataset,
+        batch_size=config["batch_size"],
+        collate_fn=ME.utils.SparseCollation(),
+        num_workers=0,
+        shuffle = True)
+
+    valid_dataset = segmentation_dataset.WaymoSegmentationDataset(root_dir='/cluster/scratch/lrabuzin/waymo_frames', mode = 'validation', device=device)
+    valid_dataloader = DataLoader(
+        valid_dataset,
+        batch_size=config["batch_size"],
+        collate_fn=ME.utils.SparseCollation(),
+        num_workers=0)
+
+    criterion = nn.CrossEntropyLoss(reduction='mean')
+    net = minkunet.MinkUNet14A(in_channels=3, out_channels=23, D=3)
+    net = net.to(device)
+    optimizer = optim.SGD(
+        net.parameters(),
+        lr=config["learning_rate"],
+        momentum=config["momentum"])
 
     wandb.watch(net)
 
-    # print('Instantiated everything')
-    for epoch in range(10):
-        # print(f"Epoch {epoch+1}")
+    for epoch in range(config["epochs"]):
         train_iter = iter(train_dataloader)
         accum_loss = 0
         accum_iter = 0
         net.train()
         for i, data in enumerate(train_iter):
-            # print(f"iter {i}")
             coords, feats, labels = data
-            # print(f"got data")
             out = net(ME.SparseTensor(feats, coords, device = device))
-            # print(f"forward pass")
             optimizer.zero_grad()
             loss = criterion(out.F.squeeze(), labels.long().to(device))
             loss.backward()
-            # print(f"backward pass")
             optimizer.step()
 
             wandb.log({"loss": loss.item()})
@@ -69,3 +98,9 @@ if __name__ == "__main__":
             accum_iter += 1
 
             print(f'Epoch:{epoch}, Iter:{i}, Loss:{accum_loss/accum_iter}')
+        
+        val_loss, iou_dict = validate_model(net, valid_dataloader, nn.CrossEntropyLoss(reduction='mean'), device=device)
+        
+        wandb.log({"validation loss": val_loss, "IOUs": iou_dict})
+        print(f'Validation loss: {val_loss}')
+        print(f'IOUs per class: {iou_dict}')
